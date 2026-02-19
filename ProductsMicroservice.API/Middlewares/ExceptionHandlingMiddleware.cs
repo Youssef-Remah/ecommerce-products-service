f4ce17cff9874c1f231badc6000b6db4b69126b7
Youssef-Remah <yremah8@gmail.com>
2026-02-19 11:22:24 +0200

    Initial Commit

---
+++ ProductsMicroservice.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿namespace ProductsMicroservice.API.Middlewares;
+
+// You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+public class ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger) : IMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
+
+    public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"{ex.GetType().ToString()}:{ex.Message}");
+
+            if (ex.InnerException is not null)
+            {
+                _logger.LogError($"{ex.InnerException.GetType().ToString()}:{ex.InnerException.Message}");
+            }
+
+            httpContext.Response.StatusCode = 500;
+
+            await httpContext.Response
+                             .WriteAsJsonAsync(new
+                             {
+                                 Message = ex.Message,
+                                 Type = ex.GetType().ToString()
+                             });
+        }
+    }
+}
+
+// Extension method used to add the middleware to the HTTP request pipeline.
+public static class ExceptionHandlingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+}
---
+++ ProductsMicroservice.API/Program.cs
@@ -0,0 +1,26 @@
+using BusinessLogic;
+using DataAccess;
+using ProductsMicroservice.API.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<ExceptionHandlingMiddleware>();
+
+builder.Services.AddDataAccess();
+
+builder.Services.AddBusinessLogic();
+
+var app = builder.Build();
+
+app.UseExceptionHandlingMiddleware();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
